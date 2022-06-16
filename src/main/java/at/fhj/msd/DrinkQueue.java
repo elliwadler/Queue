bbe245cb03d306ce8bcc977ff94651502edb9b36
Elisabeth Wadler <elisabeth.wadler@edu.fh-joanneum.at>
2022-06-16 17:48:11 +0200

    add Drink-interface and -classes

---
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -0,0 +1,28 @@
+package at.fhj.msd;
+
+public class DrinkQueue implements IQueue{
+    @Override
+    public boolean offer(String obj) {
+        return false;
+    }
+
+    @Override
+    public String poll() {
+        return null;
+    }
+
+    @Override
+    public String remove() {
+        return null;
+    }
+
+    @Override
+    public String peek() {
+        return null;
+    }
+
+    @Override
+    public String element() {
+        return null;
+    }
+}
